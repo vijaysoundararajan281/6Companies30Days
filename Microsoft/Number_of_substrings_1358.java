e6242e1b3d06d831667ead562f48c3a796b005a6
Vijay.S <20ad060@kpriet.ac.in>
2023-01-04 09:06:59 +0530

    Microsoft

---
+++ Microsoft/Number_of_substrings_1358.java
@@ -0,0 +1,13 @@
+public class Number_of_substrings_1358 {
+    
+}
+public int numberOfSubstrings(String s) {
+    int count[] = {0, 0, 0}, res = 0 , i = 0, n = s.length();
+    for (int j = 0; j < n; ++j) {
+        ++count[s.charAt(j) - 'a'];
+        while (count[0] > 0 && count[1] > 0 && count[2] > 0)
+            --count[s.charAt(i++) - 'a'];
+        res += i;
+    }
+    return res;
+}
