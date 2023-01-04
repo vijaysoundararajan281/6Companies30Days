795e2e9a4759dca1fd169c7dfaa76e02b7fd2467
Vijay.S <20ad060@kpriet.ac.in>
2023-01-04 09:09:06 +0530

    Update Number_of_substrings_1358.java

--- Microsoft/Number_of_substrings_1358.java
+++ Microsoft/Number_of_substrings_1358.java
@@ -1,13 +1,16 @@
-public class Number_of_substrings_1358 {
-    
-}
-public int numberOfSubstrings(String s) {
-    int count[] = {0, 0, 0}, res = 0 , i = 0, n = s.length();
-    for (int j = 0; j < n; ++j) {
-        ++count[s.charAt(j) - 'a'];
-        while (count[0] > 0 && count[1] > 0 && count[2] > 0)
-            --count[s.charAt(i++) - 'a'];
-        res += i;
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int freq[] = new int[3];
+        int r = 0;
+        int k = 0;
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
+                freq[s.charAt(k) - 'a']--;
+                k++;
+            }
+            r += k;
+        }
+        return r;
     }
-    return res;
 }
